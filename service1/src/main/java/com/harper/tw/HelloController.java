f6cd28afad0c43123103be94a5665d28c8ac0f2b
Haibo Wang <982255390@qq.com>
2017-07-02 12:09:35 +0800

    add fix hystrix with ribbon

--- service1/src/main/java/com/harper/tw/HelloController.java
+++ service1/src/main/java/com/harper/tw/HelloController.java
@@ -1,11 +1,13 @@
 package com.harper.tw;
 
 
+import com.harper.tw.entity.User;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,37 @@ public class HelloController {
         int sleepTime = new Random().nextInt(3000);
         Thread.sleep(sleepTime);
         return "service1";
+    }
+    @GetMapping("/hello1")
+    @ApiOperation(value = "say service1111",response = String.class)
+    public String hello11() throws InterruptedException {
+        return "service1111";
+    }
+    @GetMapping("/helloString/{str}")
+    @ApiOperation(value = "say service str",response = String.class)
+    public String helloString (@PathVariable("str") String string ) {
+        ServiceInstance serviceInstance = client.getLocalServiceInstance();
+        int sleepTime = new Random().nextInt(3000);
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "service1:"+string;
+    }
+
+    @GetMapping("/user")
+    @ApiOperation(value = "hello user",response = User.class)
+    public User helloUser() throws InterruptedException{
+        int sleepTime = new Random().nextInt(3000);
+        Thread.sleep(sleepTime);
+        return new User(sleepTime,"tw");
+    }
+    @GetMapping("/user/{id}")
+    @ApiOperation(value = "hello user",response = User.class)
+    public User helloUser(@PathVariable("id") String id) throws InterruptedException{
+        int sleepTime = new Random().nextInt(3000);
+        Thread.sleep(sleepTime);
+        return new User(Integer.valueOf(id),"tw");
     }
 }
--- service3/src/main/java/com/harper/tw/HelloController.java
+++ service3/src/main/java/com/harper/tw/HelloController.java
@@ -1,10 +1,16 @@
 package com.harper.tw;
 
+import com.netflix.hystrix.HystrixCommand.Setter;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @RestController
 public class HelloController {
@@ -12,13 +18,24 @@ public class HelloController {
     @Autowired
     private RestTemplate restTemplate;
 
-    public String helloFallBack(){
+    public String helloFallBack() {
         return "error111";
     }
 
     @RequestMapping("/hello")
     @HystrixCommand(fallbackMethod = "helloFallBack")
-    public String hello(){
-        return this.restTemplate.getForObject("http://SERVICE1/hello",String.class);
+    public String hello() {
+        return this.restTemplate.getForObject(String.format("http://%s/hello",Constants.SERVICE1_NAME), String.class);
+    }
+
+    @RequestMapping("/helloString")
+    @HystrixCommand(fallbackMethod = "helloFallBack")
+    public String helloString() {
+        return new StringCommand(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(Constants.GROUP_KEY)).andCommandKey(HystrixCommandKey.Factory.asKey(Constants.COMMAND_KEY)), restTemplate, "whb").execute();
+    }
+    @RequestMapping("/helloString1")
+    public String helloString1() throws ExecutionException, InterruptedException {
+        Future<String> future = new StringCommand(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(Constants.GROUP_KEY)).andCommandKey(HystrixCommandKey.Factory.asKey(Constants.COMMAND_KEY)), restTemplate, "whb").queue();
+        return future.get();
     }
 }
